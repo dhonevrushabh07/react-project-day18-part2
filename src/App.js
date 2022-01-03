add31325777b7a7b9a5dbf48a271f653e37e83dd
dhonevrushabh <dhonevrushabh07@gmail.com>
2022-01-03 16:32:09 +0530

    app.js modified for 3rd time

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
+/**
+ * class --in Javascript class reserved keyword
+ * because of that we have to use "className" stead of "class".  class--->className
+ * @returns
+ */
+
 export default function App() {
-  return <h1 className="bg-alert alert-success p-4">Hello Vrushabh</h1>;
+  return (
+    <div>
+      <h1 className="bg-alert alert-danger p-4 text-black">Hello World</h1>
+      <h1 className="bg-alert alert-success p-4 fs-3 text-danger">
+        Hello Universe
+      </h1>
+    </div>
+  );
 }
