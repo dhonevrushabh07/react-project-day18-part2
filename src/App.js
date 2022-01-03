db529e877b2f77f860ea86951c19a7a9438d4294
dhonevrushabh <dhonevrushabh07@gmail.com>
2022-01-03 17:36:49 +0530

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,105 +1,41 @@
 export default function App() {
+  let list = [1, 1, 1, 1, 1, 1, 1, 1];
   return (
     <div className="container-fluid p-0">
       <div className="bg-dark text-light p-2 fs-1 sticky-top">Twitter</div>
 
-      <div
-        className="row d-flex  justify-content-center align-content-center bg-alert alert-info"
-        style={{ height: "70px" }}
-      >
-        <div className="col-2   text-center ">
-          <img
-            className="rounded-circle "
-            src="https://picsum.photos/200"
-            style={{ height: "50px" }}
-            alt=""
-          />
+      {list.map((item) => (
+        <div>
+          <div
+            className="row d-flex  justify-content-center align-content-center bg-alert alert-info"
+            style={{ height: "70px" }}
+          >
+            <div className="col-2   text-center ">
+              <img
+                className="rounded-circle "
+                src="https://picsum.photos/00"
+                style={{ height: "50px" }}
+                alt=""
+              />
+            </div>
+            <div className="col-10  fs-1">Vrushabh Dhone</div>
+          </div>
+          <div className="bg-alert alert-danger p-2 mt-1 fw-bold">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
+            libero ratione facere, sunt nesciunt blanditiis. Modi dolor aut
+            itaque beatae exercitationem, vel, temporibus laudantium ullam vero
+            totam suscipit dolore quos incidunt dolorum officia non aperiam
+            doloremque veritatis eveniet? Earum cumque aperiam nesciunt.
+            Accusamus, dolore cupiditate dolor consequuntur earum enim
+            reprehenderit quae voluptate minus, voluptas, suscipit optio quaerat
+            fugiat maxime deserunt dignissimos soluta nihil cumque. Aut vitae
+            pariatur saepe vel blanditiis adipisci et. Molestiae nostrum facere,
+            nulla illum vel nam praesentium deserunt aut vitae minima earum sunt
+            voluptatum doloribus in quod illo eum omnis quae, nesciunt placeat
+            ducimus! Sed, dolore temporibus?
+          </div>
         </div>
-        <div className="col-10  fs-1">Vrushabh Dhone</div>
-      </div>
-      <div className="bg-alert alert-danger p-2 mt-1 fw-bold">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-        nulla ducimus ad quis ipsam cupiditate commodi temporibus dicta quae!
-        Sunt optio beatae ab minima vitae quisquam at error nam aliquid!
-      </div>
-
-      <div
-        className="row d-flex  justify-content-center align-content-center bg-alert alert-info"
-        style={{ height: "70px" }}
-      >
-        <div className="col-2   text-center ">
-          <img
-            className="rounded-circle "
-            src="https://picsum.photos/200"
-            style={{ height: "50px" }}
-            alt=""
-          />
-        </div>
-        <div className="col-10  fs-1">Vrushabh Dhone</div>
-      </div>
-      <div className="bg-alert alert-danger p-2 mt-1 fw-bold">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-        nulla ducimus ad quis ipsam cupiditate commodi temporibus dicta quae!
-        Sunt optio beatae ab minima vitae quisquam at error nam aliquid!
-      </div>
-
-      <div
-        className="row d-flex  justify-content-center align-content-center bg-alert alert-info"
-        style={{ height: "70px" }}
-      >
-        <div className="col-2   text-center ">
-          <img
-            className="rounded-circle "
-            src="https://picsum.photos/200"
-            style={{ height: "50px" }}
-            alt=""
-          />
-        </div>
-        <div className="col-10  fs-1">Vrushabh Dhone</div>
-      </div>
-      <div className="bg-alert alert-danger p-2 mt-1 fw-bold">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-        nulla ducimus ad quis ipsam cupiditate commodi temporibus dicta quae!
-        Sunt optio beatae ab minima vitae quisquam at error nam aliquid!
-      </div>
-      <div
-        className="row d-flex  justify-content-center align-content-center bg-alert alert-info"
-        style={{ height: "70px" }}
-      >
-        <div className="col-2   text-center ">
-          <img
-            className="rounded-circle "
-            src="https://picsum.photos/200"
-            style={{ height: "50px" }}
-            alt=""
-          />
-        </div>
-        <div className="col-10  fs-1">Vrushabh Dhone</div>
-      </div>
-      <div className="bg-alert alert-danger p-2 mt-1 fw-bold">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-        nulla ducimus ad quis ipsam cupiditate commodi temporibus dicta quae!
-        Sunt optio beatae ab minima vitae quisquam at error nam aliquid!
-      </div>
-      <div
-        className="row d-flex  justify-content-center align-content-center bg-alert alert-info"
-        style={{ height: "70px" }}
-      >
-        <div className="col-2   text-center ">
-          <img
-            className="rounded-circle "
-            src="https://picsum.photos/200"
-            style={{ height: "50px" }}
-            alt=""
-          />
-        </div>
-        <div className="col-10  fs-1">Vrushabh Dhone</div>
-      </div>
-      <div className="bg-alert alert-danger p-2 mt-1 fw-bold">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-        nulla ducimus ad quis ipsam cupiditate commodi temporibus dicta quae!
-        Sunt optio beatae ab minima vitae quisquam at error nam aliquid!
-      </div>
+      ))}
     </div>
   );
 }
