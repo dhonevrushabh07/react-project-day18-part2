4b330d5a6f6e5bfc4fdba87ff2a44ed617529307
dhonevrushabh <dhonevrushabh07@gmail.com>
2022-01-03 17:03:37 +0530

    b4 updated app.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,104 @@
-/**
- * style=""
- * style="background-color:red"
- *
- *
- * Rule
- * style={{backgroundColor:"red"}}
- *
- * 1)First two letters CSS property are now Converted into camelcase property
- * 2)The value of the property must be in "Double Qoute"
- */
-
 export default function App() {
   return (
-    <div>
+    <div className="container-fluid p-0">
+      <div className="bg-dark text-light p-2 fs-1 sticky-top">Twitter</div>
+
       <div
-        style={{
-          height: "50px",
-          backgroundColor: "royalblue",
-          fontWeight: "bolder",
-          fontSize: "20px",
-          padding: "5px",
-          textAlign: "center",
-          color: "white",
-        }}
+        className="row d-flex  justify-content-center align-content-center bg-alert alert-info"
+        style={{ height: "70px" }}
       >
-        Hello Guys
+        <div className="col-2   text-center ">
+          <img
+            className="rounded-circle "
+            src="https://picsum.photos/200"
+            style={{ height: "50px" }}
+            alt=""
+          />
+        </div>
+        <div className="col-10  fs-1">Vrushabh Dhone</div>
+      </div>
+      <div className="bg-alert alert-danger p-2 mt-1 fw-bold">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        nulla ducimus ad quis ipsam cupiditate commodi temporibus dicta quae!
+        Sunt optio beatae ab minima vitae quisquam at error nam aliquid!
+      </div>
+
+      <div
+        className="row d-flex  justify-content-center align-content-center bg-alert alert-info"
+        style={{ height: "70px" }}
+      >
+        <div className="col-2   text-center ">
+          <img
+            className="rounded-circle "
+            src="https://picsum.photos/200"
+            style={{ height: "50px" }}
+            alt=""
+          />
+        </div>
+        <div className="col-10  fs-1">Vrushabh Dhone</div>
+      </div>
+      <div className="bg-alert alert-danger p-2 mt-1 fw-bold">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        nulla ducimus ad quis ipsam cupiditate commodi temporibus dicta quae!
+        Sunt optio beatae ab minima vitae quisquam at error nam aliquid!
+      </div>
+
+      <div
+        className="row d-flex  justify-content-center align-content-center bg-alert alert-info"
+        style={{ height: "70px" }}
+      >
+        <div className="col-2   text-center ">
+          <img
+            className="rounded-circle "
+            src="https://picsum.photos/200"
+            style={{ height: "50px" }}
+            alt=""
+          />
+        </div>
+        <div className="col-10  fs-1">Vrushabh Dhone</div>
+      </div>
+      <div className="bg-alert alert-danger p-2 mt-1 fw-bold">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        nulla ducimus ad quis ipsam cupiditate commodi temporibus dicta quae!
+        Sunt optio beatae ab minima vitae quisquam at error nam aliquid!
+      </div>
+      <div
+        className="row d-flex  justify-content-center align-content-center bg-alert alert-info"
+        style={{ height: "70px" }}
+      >
+        <div className="col-2   text-center ">
+          <img
+            className="rounded-circle "
+            src="https://picsum.photos/200"
+            style={{ height: "50px" }}
+            alt=""
+          />
+        </div>
+        <div className="col-10  fs-1">Vrushabh Dhone</div>
+      </div>
+      <div className="bg-alert alert-danger p-2 mt-1 fw-bold">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        nulla ducimus ad quis ipsam cupiditate commodi temporibus dicta quae!
+        Sunt optio beatae ab minima vitae quisquam at error nam aliquid!
+      </div>
+      <div
+        className="row d-flex  justify-content-center align-content-center bg-alert alert-info"
+        style={{ height: "70px" }}
+      >
+        <div className="col-2   text-center ">
+          <img
+            className="rounded-circle "
+            src="https://picsum.photos/200"
+            style={{ height: "50px" }}
+            alt=""
+          />
+        </div>
+        <div className="col-10  fs-1">Vrushabh Dhone</div>
+      </div>
+      <div className="bg-alert alert-danger p-2 mt-1 fw-bold">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        nulla ducimus ad quis ipsam cupiditate commodi temporibus dicta quae!
+        Sunt optio beatae ab minima vitae quisquam at error nam aliquid!
       </div>
     </div>
   );
