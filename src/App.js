ffea252245b215e3bbdf9705df7a38cf6d198c7c
dhonevrushabh <dhonevrushabh07@gmail.com>
2022-01-03 22:43:58 +0530

    part3 b3 app.js updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ export default function App() {
     <div>
       <HelloUniverse></HelloUniverse>
       <HelloUniverse />
+      <Vrushabh></Vrushabh>
+      <Vrushabh />
     </div>
   );
 }
@@ -46,6 +48,33 @@ function HelloUniverse() {
         type="button"
         value="Dislike"
       />
+    </div>
+  );
+}
+
+export function Vrushabh() {
+  return (
+    <div>
+      <div
+        className="bg-info p-2 fs-1 fw-bolder text-light mt-1"
+        style={{ height: "70px", borderBottom: "black solid" }}
+      >
+        Hello Vrushabh Its your Tag
+      </div>
+      <p className="bg-secondary text-light p-2 font-monospace">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+        praesentium illo architecto enim rerum ratione fugiat quaerat cumque
+        velit nihil vitae suscipit commodi, quia necessitatibus odit doloremque
+        at provident accusamus reprehenderit iste atque recusandae! Nemo fugit
+        doloribus reprehenderit reiciendis sint voluptatem labore eaque magnam
+        provident minus recusandae adipisci tempora, eligendi aspernatur
+        architecto. Repudiandae modi doloremque molestiae rem, officia ipsum
+        commodi id velit ipsam corporis recusandae quisquam enim qui odio! Id
+        facilis fugiat ea. Laborum quia repellendus ea quam optio aspernatur
+        debitis eligendi molestiae minus tenetur! Magnam, earum, voluptatum
+        pariatur, voluptatem id ex numquam voluptates repudiandae aut ullam
+        aperiam repellendus quo?
+      </p>
     </div>
   );
 }
